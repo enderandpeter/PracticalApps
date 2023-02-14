387967cad3ce150f9b09294f35f723c45b5ab40d
Spencer Williams <spencer@aninternetpresence.net>
2023-02-14 00:32:01 -0700

    Layout is not being applied to Razor pages

---
+++ Northwind.Web/Program.cs
@@ -0,0 +1,8 @@
+﻿using Northwind.Web; // Startup
+Host.CreateDefaultBuilder(args)
+  .ConfigureWebHostDefaults(webBuilder =>
+  {
+    webBuilder.UseStartup<Startup>();
+
+  }).Build().Run();
+Console.WriteLine("This executes after the web server has stopped!");
